27f485b7c706a5fccce1cccf76fa20974f8f6d8f
xcode17 <do.crazy192@gmail.com>
2022-08-18 22:39:32 +0700

    add: test component

---
+++ src/pages/Test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Test = () => {
+    return (
+        <>
+            <div className="w-full py-5 text-center text-lg">
+                <h1 className="font-semibold text-blue-700">Hello react</h1>
+            </div>
+        </>
+    );
+};
+
+export default Test;
